8fab06a4b2bed89fde1ea1b1e9e0bdcc18692e8c
Soung jin Ryu <neocat@kakao.com>
2020-12-19 22:12:19 +0900

    다익스트라 알고리즘

--- 다익스트라.py
+++ 다익스트라.py
@@ -17,51 +17,6 @@ def findSort(거리,통과지점):
             index = i
     return index
 
-def hepSort(data,통과지점):
-    cp = []
-    skeep
-    for i in len(data):
-        cp.append((i,data))
-    for j in 통과지점:
-        if j:
-            skeep+=1
-    i = len(data)-1
-    tmp = 0
-    
-    makeHeap(cp)
-    '''
-    downHeap(cp,i-1,0)
-    for j in range(i,0):
-        tmp = cp[0]
-        cp[0] = data[i]
-        cp[i] = tmp
-        downHeap(cp,i-1, 0)
-    '''
-    return cp
-        
-def makeHeap(data):
-    i = int(len(data)/2)
-    for j in range(i,0):
-        downHeap(data,n,i)
-    
-def downHeap(list,k):
-    index = 2*k
-    tmp = 0
-
-    if len(list) <= index:
-        return
-
-    if len(list) <= index+1 and list[index] > list[index + 1]:
-        index+= 1
-
-    i,j = list[index]
-    i,l = list[k]
-    if j < l:
-        tmp = list[index]
-        list[index] = list[k]
-        list[k] = tmp
-        downHeap(list,n,index)
-
 def Dijkstra(비용, 시작지점):
     거리 = [0 for i in range(len(비용))]
     #탐색여부
@@ -74,8 +29,7 @@ def Dijkstra(비용, 시작지점):
     s[시작지점] = 1
     거리[시작지점 ] = 0
     for i in range(1,len(비용)):
-        #u = findSort(거리,s)
-        
+        u = findSort(거리,s)
         """
         print("최소거리 : " + str(u))
         print("경로 : " + str(r))
@@ -92,6 +46,7 @@ def Dijkstra(비용, 시작지점):
                 거리[j] = 거리[u] + 비용[u][j]
                 r[j] = u
         
+        print("="*50)
     return (r, 거리)
 
 n = 20
@@ -107,12 +62,6 @@ for i in 비용:
     print(str(i))
 print("="*50)
 
-for i in 비용:
-    makeHeap(i)
-    print(i)
-    print("="*50)
-
-'''
 j ,k = Dijkstra(비용,0)
 print("="*50)
 print("경로")
@@ -120,4 +69,3 @@ print(j)
 print("="*50)
 print("최단거리")
 print(k)
-'''
